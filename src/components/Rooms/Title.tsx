d3c928ac201410eef3d0a6cdae2c5c7908efa052
Jubril Musa <41811898+Muhsar@users.noreply.github.com>
2022-06-09 03:41:56 +0100

    almost done with Bursar page

--- src/components/Rooms/Title.tsx
+++ src/components/Rooms/Title.tsx
@@ -3,6 +3,14 @@ import { ChevronDownIcon, SearchIcon, SortAscendingIcon } from '@heroicons/react
 import React from 'react'
 
 const Title = () => {
+  // type,
+  //     image,
+  //     video,
+  //     room_number,
+  //     number_acceptable,
+  //     hostel_name,
+  //     gender,
+  //     price
     const [open, setOpen] = React.useState(false)
     return (
         <div className="pb-5 border-b border-green-200 sm:flex sm:items-center sm:justify-between">
@@ -12,7 +20,7 @@ const Title = () => {
             Search
           </label>
           <div className="flex rounded-md shadow-sm">
-            <div className="relative flex-grow focus-within:z-10">
+            <div className="relative flex-grow focus-within:z-10 mr-5">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <SearchIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
               </div>
@@ -33,16 +41,14 @@ const Title = () => {
                 // onChange={handleSearch}
               />
             </div>
+            <div className="flex items-center h-full">
             <button
               type="button"
-              className="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 border border-green-300 rounded-r-md bg-green-50 hover:bg-green-100 focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-green-500"
-            >
+              className="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium border border-green-600 rounded-r-md bg-green-600 hover:bg-green-100 hover:text-green-700 text-white focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-green-500"
+              >
             Add New
-              {/* <SortAscendingIcon className="w-5 h-5 text-gray-400" aria-hidden="true" /> */}
-              {/* <span className="ml-2">Sort</span> */}
-              {/* <ChevronDownIcon className="ml-2.5 -mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" /> */}
             </button>
-            {/* <SlideOver title="Add Students" Component={Component} open={open} setOpen={setOpen} /> */}
+              </div>
           </div>
         </div>
       </div>
